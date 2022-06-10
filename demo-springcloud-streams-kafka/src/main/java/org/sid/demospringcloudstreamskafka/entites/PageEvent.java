6c840f8cc59ed1d63accbec22fd0445acf620a40
rkiazohari <81563806+rkiazohari@users.noreply.github.com>
2022-06-10 23:42:03 +0200

    Add files via upload

---
+++ demo-springcloud-streams-kafka/src/main/java/org/sid/demospringcloudstreamskafka/entites/PageEvent.java
@@ -0,0 +1,21 @@
+package org.sid.demospringcloudstreamskafka.entites;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class PageEvent {
+private String name;
+private  String user;
+private Date date;
+private  long duration;
+
+
+
+
+}
